5ac660c61a017652dbbbf26edf12eac66c168337
Sean Antony <sean.antony@virginmediao2.co.uk>
2023-05-19 20:50:23 +0100

    Update version

--- Sources/iOSAppBuildPipelineActionsLib/Commands/Version.swift
+++ Sources/iOSAppBuildPipelineActionsLib/Commands/Version.swift
@@ -9,6 +9,6 @@ public final class Version<T>: NSObject where T: RedactableTextOutputStream {
     }
     
     public func version() {
-        print("2.2.1-alpha.7", to: &textOutputStream)
+        print("3.0.0", to: &textOutputStream)
     }
 }
